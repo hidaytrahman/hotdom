47dad108075afb4850bc5cf933e19d13a5d8d7a3
Hidayt Rahman <hidaytrahman@gmail.com>
2021-06-28 22:35:55 +0530

    access together
    import dom, { isIE , getLocalStorage, loadScript, getAllLocalStorage, getLang, currentLanguage} from "hotdom";

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 import hotdom from "./lib/hotdom.js";
+import { convertArrToObj, getUrlParameter, loadScript } from "./lib/utils.js";
+import { getLocalStorage, isIE, getAllLocalStorage, getLang, currentLanguage } from "./lib/browser";
+
+export {
+    // utils
+    convertArrToObj,
+    getUrlParameter,
+    loadScript,
+
+    // Browser utils
+    getLocalStorage,
+    getAllLocalStorage,
+    isIE,
+    getLang,
+    currentLanguage
+};
 
 export default hotdom;
